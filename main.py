cd7cbb4b085641ca340b22bee155cb20c9fe8924
Andrew Gaylord <113562174+AGaylord6@users.noreply.github.com>
2024-05-13 20:06:37 -0400

    updates noise

--- main.py
+++ main.py
@@ -32,25 +32,27 @@ EOMs are messed up. changed rw_config back to gamma and such
     change I_trans from non-zero?
 
 must ensure quaternion is normalized
+
 wrong orientation after hfuncing??
 how is B_true connected to our starting state + initial eoms progagation
 
-do we trust our process or measurements more?
-    should be similar?
+something is bugged in generating data/filtering
 
 generate fake imu data using matlab functionality??
 
+flesh out 3D graphs more: colors, many at once (ideal, data, filtered)
+
 '''
 
-def plot3D(vectors, plotSegment):
+def plot3DVectors(vectors, plotSegment):
     # plotSegment: 3 digit number 'nmi'
     #       split plot into n by m plots and picks ith one
     #       ex: 121 splits into 1 row, 2 columns and picks the first (the left half)
 
-    bounds = [-2, 2]
+    bounds = [-1.5, 1.5]
 
     result = np.array([np.concatenate(([0, 0, 0], vectors[0]))])
-    print(result)
+    # print(result)
 
     for i in range (1, len(vectors)):
         v = vectors[i]
@@ -78,16 +80,57 @@ def plot3D(vectors, plotSegment):
 
     plt.show()
 
+
+def plotData(data, numVectors, plotSegment):
+    # print numVector elements of data, divided evenly
+
+    section = int(len(data) / (2*numVectors))
+
+    result = np.array([data[0][:3]])
+
+    for i in range(1, numVectors):
+        result = np.append(result, np.array([data[i*section][:3]]), axis=0)
+    
+    print(result)
+
+    # plot3DVectors(np.array([ukf.B_true, data[50][:3], data[100][:3], data[150][:3]]), 121)
+    plot3DVectors(result, 111)
+
+def euler_from_quaternion(w, x, y, z):
+        """
+        Convert a quaternion into euler angles (roll, pitch, yaw)
+        roll is rotation around x in radians (counterclockwise)
+        pitch is rotation around y in radians (counterclockwise)
+        yaw is rotation around z in radians (counterclockwise)
+        """
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+     
+        return roll_x, pitch_y, yaw_z # in radians
+
+
 if __name__ == "__main__":
     
 
-    ukf = Filter(200, 0.1, 7, 6, 0, 0, np.array([1, 0, 0]), np.array([0, 0, 0]), UKF)
+    ukf = Filter(200, 0.1, 7, 6, 0, 0, np.array([1, 1, 1]), np.array([0, 0, 0]), UKF)
 
     # set process noise
     ukf.ukf_setQ(.005, 10)
 
     # set measurement noise
     ukf.ukf_setR(.2, .02)
+    ukf.ukf_setR(6, 6)
+
 
     ideal_reaction_speeds = ukf.generateSpeeds(1000, -1000, ukf.n/2, 100, np.array([0, 0, 1]))
     # print(ideal_reaction_speeds[:20])
@@ -101,17 +144,21 @@ if __name__ == "__main__":
     data = ukf.generateData(ideal, magNoises, gyroNoises, 0)
     # print("data: ", data[:20])
 
-    # number of vectors to plot
-    numVectors = 5
+    # # plot3DVectors(np.array([ukf.B_true, data[50][:3], data[100][:3], data[150][:3]]), 121)
+    # plot3DVectors(result, 111)
 
-    plot3D(np.array([ukf.B_true, data[50][:3], data[100][:3], data[150][:3]]), 121)
+    # plotData(data, 5, 111)
+
+    # ideal_xyz = [np.matmul(quaternion_rotation_matrix(x), np.array([1, 0, 0])) for x in ideal]
+    # plotData(ideal_xyz, 3, 111)
+
 
 
     filtered = ukf.simulate(data, ideal_reaction_speeds)
 
 
     # ukf.plotResults(ideal)
-    # ukf.plotResults(filtered)
+    ukf.plotResults(filtered)
 
 
 
